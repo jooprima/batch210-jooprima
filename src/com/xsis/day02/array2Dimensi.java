544334a344077009779333074691e50edb4f222c
jooprima <jooprima@gmail.com>
2019-08-29 15:53:23 +0700

    practice array multidimensi 2

---
+++ src/com/xsis/day02/array2Dimensi.java
@@ -0,0 +1,20 @@
+package com.xsis.day02;
+
+public class array2Dimensi {
+    public static void main(String[] args) {
+        int[][] nums = {{1, 5, 4, 3},
+                        {9, 3, 5, 6},
+                        {4, 5, 6, 12}};
+        array2Dimensi array2d = new array2Dimensi();
+        array2d.searchNums(nums,6);
+    }
+
+    public void searchNums(int[][] nums, int search) {
+        for (int row = 0; row < nums.length; row++) {
+            for (int column = 0; column < nums[row].length; column++) {
+                if (nums[row][column] == search)
+                    System.out.println("Search : " + search + ", at position " + "Baris : " + row + " ,Kolom : " + column);
+            }
+        }
+    }
+}
