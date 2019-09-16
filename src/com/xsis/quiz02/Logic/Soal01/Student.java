437a689bd45b86a7717c4ba88339de6deccf4932
jooprima <jooprima@gmail.com>
2019-09-16 20:47:11 +0700

    quiz-02-student

--- src/com/xsis/quiz02/Logic/Soal01/Student.java
+++ src/com/xsis/quiz02/Logic/Soal01/Student.java
@@ -1,4 +1,56 @@
 package com.xsis.quiz02.Logic.Soal01;
 
 public class Student {
+    private String nim;
+    private String nama;
+    private String jurusan;
+    static int totalStudent = 0;
+
+    public Student(String nim, String nama, String jurusan) {
+        this.nim = nim;
+        this.nama = nama;
+        this.jurusan = jurusan;
+        totalStudent++;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public static int getTotalStudent() {
+        return totalStudent;
+    }
+
+    public static void setTotalStudent(int totalStudent) {
+        Student.totalStudent = totalStudent;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "nim='" + nim + '\'' +
+                ", nama='" + nama + '\'' +
+                ", jurusan='" + jurusan + '\'' +
+                '}';
+    }
 }
--- src/com/xsis/quiz02/Logic/Soal01/StudentDemo.java
+++ src/com/xsis/quiz02/Logic/Soal01/StudentDemo.java
@@ -2,6 +2,12 @@ package com.xsis.quiz02.Logic.Soal01;
 
 public class StudentDemo {
     public static void main(String[] args) {
+        Student std1 = new Student("123","joo","informatika");
+        Student std2 = new Student("124","prima","komputer");
+        Student std3 = new Student("125","yohanes","akuntansi");
+        Student std4 = new Student("126","trisnanto","manajemen");
+//        Student std5 = new Student("127","ucup","informatika");
 
+        System.out.println("Total Student = " + Student.totalStudent);
     }
 }
